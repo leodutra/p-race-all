90fdd7843b405104959e44d060c534244a4f95c1
Leonardo Dutra <leodutra.br@gmail.com>
2020-05-29 01:05:50 -0300

    Simplified code, with better speed.

--- index.js
+++ index.js
@@ -1,39 +1,13 @@
-module.exports = function raceAll (promises) {
-    promises = promises.slice()
-    const result = []
+module.exports = function raceAll(promises) {
+    const results = []
+    const functions = []
     for (let i = 0, l = promises.length; i < l; i++) {
-        result.push(
-            new Promise((resolve, reject) => {
-                let first = true
-                const ok = k => data => {
-                    if (first) {
-                        first = false
-                        promises.splice(k, 1)
-                        resolve(data)
-                    }
-                }
-                const err = k => error => {
-                    if (first) {
-                        first = false
-                        promises.splice(k, 1)
-                        reject(error)
-                    }
-                }
-                if (result[i-1]) {
-                    result[i-1].finally(() => {
-                        promises.map((p, k) => {
-                            p.then(ok(k))
-                            p.catch(err(k))
-                        })
-                    })
-                } else {
-                    promises.map((p, k) => {
-                        p.then(ok(k))
-                        p.catch(err(k))
-                    })
-                }
-            })
+        results.push(
+            new Promise((resolve, reject) => functions.push({ resolve, reject }))
         )
+        Promise.resolve(promises[i])
+            .then(value => functions.shift().resolve(value))
+            .catch(error => functions.shift().reject(error))
     }
-    return result
+    return results
 }
