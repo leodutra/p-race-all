da0000bdef2fb360dd98f9630dcbc21597249cf2
Leo Dutra <leodutra.br@gmail.com>
2022-01-11 19:57:12 -0300

    Rename internal vars

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 module.exports = function raceAll(promises) {
-    const results = []
-    const functions = []
+    const outputPromises = []
+    const fulfillmentList = []
     for (let i = 0, l = promises.length; i < l; i++) {
-        results.push(
-            new Promise((resolve, reject) => functions.push({ resolve, reject }))
+        outputPromises.push(
+            new Promise((resolve, reject) => fulfillmentList.push({ resolve, reject }))
         )
         Promise.resolve(promises[i])
-            .then(value => functions.shift().resolve(value))
-            .catch(error => functions.shift().reject(error))
+            .then(value => fulfillmentList.shift().resolve(value))
+            .catch(error => fulfillmentList.shift().reject(error))
     }
-    return results
+    return outputPromises
 }
